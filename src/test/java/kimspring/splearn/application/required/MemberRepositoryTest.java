b53a53470c17f6e422c4499026bea074e609e8f5
김윤혁 <kyhyeok@gmail.com>
2025-07-29 10:37:30 +0900

    엔티티의 자연키 지정

--- src/test/java/kimspring/splearn/application/required/MemberRepositoryTest.java
+++ src/test/java/kimspring/splearn/application/required/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import static kimspring.splearn.domain.MemberFixture.createMemberRegisterRequest;
 import static kimspring.splearn.domain.MemberFixture.createPasswordEncoder;
@@ -31,5 +32,15 @@ class MemberRepositoryTest {
         assertThat(member.getId()).isNotNull();
 
         entityManager.flush();
+    }
+
+    @Test
+    void duplicateEmailFail() {
+        Member member = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+        memberRepository.save(member);
+
+        Member member2 = Member.register(createMemberRegisterRequest(), createPasswordEncoder());
+        assertThatThrownBy(() -> memberRepository.save(member2))
+                .isInstanceOf(DataIntegrityViolationException.class);
     }
 }
