56eebdc573619d9295e676c70ee605f36c18c8fe
김윤혁 <kyhyeok@gmail.com>
2025-07-31 18:03:15 +0900

    엔티티 클래스와 JPA 매핑 정보 분리

--- src/main/java/kimspring/splearn/domain/Member.java
+++ src/main/java/kimspring/splearn/domain/Member.java
@@ -1,6 +1,6 @@
 package kimspring.splearn.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -15,13 +15,7 @@ import static org.springframework.util.Assert.state;
 @Getter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@NaturalIdCache
-public class Member {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Embedded
+public class Member extends AbstractEntity {
     @NaturalId
     private Email email;
 
@@ -29,7 +23,6 @@ public class Member {
 
     private String passwordHash;
 
-    @Enumerated(EnumType.STRING)
     private MemberStatus status;
 
     public static Member register(MemberRegisterRequest registerRequest, PasswordEncoder passwordEncoder) {
