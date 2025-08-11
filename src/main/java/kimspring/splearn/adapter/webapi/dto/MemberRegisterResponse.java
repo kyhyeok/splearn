2d064a47b56c692db9498dca42bb2ab8620e5e33
김윤혁 <kyhyeok@gmail.com>
2025-08-11 12:56:57 +0900

    API 테스트와 ProblemDetail 예외 핸들러 개발

--- src/main/java/kimspring/splearn/adapter/webapi/dto/MemberRegisterResponse.java
+++ src/main/java/kimspring/splearn/adapter/webapi/dto/MemberRegisterResponse.java
@@ -2,7 +2,7 @@ package kimspring.splearn.adapter.webapi.dto;
 
 import kimspring.splearn.domain.member.Member;
 
-public record MemberRegisterResponse(Long memberId, String emailAddress) {
+public record MemberRegisterResponse(Long memberId, String email) {
     public static MemberRegisterResponse of(Member member) {
         return new MemberRegisterResponse(member.getId(), member.getEmail().address());
     }
